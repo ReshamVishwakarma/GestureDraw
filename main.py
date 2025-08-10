eeebe4228f7b569cb8222a293ab330e5b7c1625f
Resham Vishwakarma <reshamofficial123@gmail.com>
2025-08-10 07:11:51 +0100

    load palette images and define the color list

--- main.py
+++ main.py
@@ -17,3 +17,26 @@ mpdraw = mp.solutions.drawing_utils
 
 pasttime = 0
 
+folder = 'colors'
+mylist = os.listdir(folder)
+overlist = []
+
+#BGR color palette
+color_list = [
+    (0, 0, 0),          #Eraser - Black
+    (255, 0 , 255),     #Pink
+    (0, 0, 255),        #Red
+    (0, 255, 0),        #Green        
+    (255, 0 , 0),       #Blue
+    (0, 255, 255),      #Yellow
+    (0, 127, 255),      #Orange
+    (255, 0, 127)       #Purple
+
+]
+
+for img_name in mylist:
+    image = cv2.imread(f'{folder}/{img_name}')
+    overlist.append(image)
+
+header = overlist[0]
+col = color_list[0]    
